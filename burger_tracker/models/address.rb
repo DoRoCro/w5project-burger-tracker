8d452b68beb752e1de1b279ed5bd735c8a7b6846
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-25 14:05:08 +0000

    define Burger basics + .to_i id in address

--- burger_tracker/models/address.rb
+++ burger_tracker/models/address.rb
@@ -3,7 +3,7 @@ class Address < Crud
 
   attr_reader :id, :street_addr, :street, :area, :city, :post_code, :telephone
   def initialize(options)
-    @id = options['id'] if options['id']
+    @id = options['id'].to_i if options['id']
     @street_addr = options['street_addr']
     @street = options['street']
     @area = options['area']
---
+++ burger_tracker/models/burger.rb
@@ -0,0 +1,14 @@
+require_relative('../db/crud.rb')
+class Burger < Crud
+
+  attr_reader :id, :name, :price, :restaurant_id, :preference
+  
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @price = options['price'].to_f
+    @restaurant_id = options['restaurant_id'].to_i
+    @preference = options['preference'].to_i
+  end
+
+end
