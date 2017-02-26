d19c431cc770fa6f5c24ddc1271aa908dd0dae36
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-26 21:45:02 +0000

    add some initial styles for web server

---
+++ burger_tracker/app.rb
@@ -0,0 +1,10 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+# require_relative('controllers/restaurants_controller')
+# require_relative('controllers/deals_controller')
+# require_relative('controllers/calendar_controller')
+
+set :port, 4568
+get '/' do
+  erb( :index )
+end
