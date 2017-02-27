1db4c7e8f33bb9cbcd69ad1e9b95178a057402bd
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-27 13:38:57 +0000

    add empty restaurant add form

--- burger_tracker/controllers/restaurants_controller.rb
+++ burger_tracker/controllers/restaurants_controller.rb
@@ -16,7 +16,6 @@ post '/restaurants' do
   # do an update with form data
 end
 
-
 get '/restaurants/:id' do
   # show individual restaurant
   # with options to edit, delete
@@ -25,7 +24,27 @@ get '/restaurants/:id' do
 end
 
 get '/restaurants/:id/edit' do
-  # edit form for update
+  # edit form for update restaurant details
   erb ( :"restaurants/edit" )
+end
 
+get '/restaurants/:id/burgers/new' do
+  # edit form for new burger for given restaurant
+  erb ( :"restaurants/burgers/new" )
+end
+
+post '/restaurants/:id/burgers' do
+  # edit form for new burger for given restaurant
+  erb ( :"restaurants/burgers/new" )
+end
+
+
+get '/restaurants/:id/deals' do
+  # list deals for restaurant
+  erb ( :"restaurants/deals/index" )
+end
+
+get '/restaurants/:id/deals/new' do
+  # form to add new deal for restaurant
+  erb ( :"restaurants/deals/new" )
 end
